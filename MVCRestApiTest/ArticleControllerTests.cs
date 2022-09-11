afc08caa6a9e52e348adffe8c9ed20b1b559e7b7
ahkrisztian <lunarcsik.krisztian@gmail.com>
2022-09-11 14:07:10 +0200

    UnitTesting

--- MVCRestApiTest/ArticleControllerTests.cs
+++ MVCRestApiTest/ArticleControllerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using MVCRestAPI.Controllers;
 using MVCRestAPI.Data;
+using MVCRestAPI.DTOs;
 using MVCRestAPI.Models;
 using MVCRestAPI.Profiles;
 using System;
@@ -10,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MVCRestApiTest
 {
@@ -19,6 +21,7 @@ namespace MVCRestApiTest
         ArticleProfiles realProfile;
         MapperConfiguration config;
         IMapper mapper;
+
         public ArticleControllerTests()
         {
             mockRepo = new Mock<IArticleAPIRepo>();
@@ -46,6 +49,62 @@ namespace MVCRestApiTest
             //Act
             var result = article.GetArticles();
 
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetArticles_ReturnsItem_WhenDBHasONeItem()
+        {
+            //Arrange
+
+            mockRepo.Setup(repo => repo.GetAllArticles()).Returns(GetArticles(1));
+
+            var article = new ArticleController(mockRepo.Object, mapper);
+
+            //Act
+            var result = article.GetArticles();
+
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+
+            var articles = okResult.Value as List<ArticleReadDTO>;
+
+            Assert.Single(articles);
+        }
+
+        [Fact]
+
+        public void GetArticleById_Returns404NotFound_WhenNonExistentIdProvided()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.GetArticleById(0)).Returns(() => null);
+
+            var controller = new ArticleController(mockRepo.Object, mapper);
+
+
+            //Act
+            var result = controller.GetArticleById(0);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetArticleByID_Returns200OK__WhenValidIDProvided()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+            repo.GetArticleById(1)).Returns(new Article
+            {
+                Id = 1,
+                Text = "mock",
+                Title = "Mock",
+                Type = "Mock"
+            });
+            var controller = new ArticleController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetArticleById(1);
             //Assert
             Assert.IsType<OkObjectResult>(result.Result);
         }
